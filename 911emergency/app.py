1109f3f83892c75174a4af727ea0be42cd43bc1b
manika15 <manika.kapoor90@gmail.com>
2017-04-21 20:57:55 -0700

    functions removed from app.py

--- 911emergency/app.py
+++ 911emergency/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, make_response, flash, redirect, session, abort, jsonify
-from pandas import *
-import datetime as dt
 import json
-import os
 from urllib.request import urlopen
+from functions import *
 
 app = Flask(__name__)
 
@@ -22,22 +20,9 @@ def emergency():
         result = json.loads(emergency_overview(year))
         return jsonify(result)
 
-# read 911 csv
-cwd = os.getcwd() + '/' + 'montgomeryPA_911.csv'
-data = read_csv(cwd)
-pandas.to_datetime(data['timeStamp'])
-def emergency_overview(year):
-    overview_data = data[data['timeStamp'].str.contains(year)]
-    overview_data_ems = overview_data[overview_data['title'].str.contains('EMS:')]
-    overview_data_fire = overview_data[overview_data['title'].str.contains('Fire:')]
-    overview_data_traffic = overview_data[overview_data['title'].str.contains('Traffic:')]
-    count_ems = len(overview_data_ems.index)
-    count_fire = len(overview_data_fire.index)
-    count_traffic = len(overview_data_traffic.index)
-    dictionary = {"EMS": count_ems, "Fire": count_fire, "Traffic": count_traffic}
-    dictionary = [{"label": i , "value": j} for i,j in dictionary.items()]
-    result = json.dumps(dictionary)
-    return result
+#print(emergency_trend_comparison('2016', 'EMS:', 'ASSAULT VICTIM', 'CARDIAC EMERGENCY'))
+#print (emergency_trend('2016', 'EMS:', 'ASSAULT VICTIM'))
+#print (emergency_overview('2016'))
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
