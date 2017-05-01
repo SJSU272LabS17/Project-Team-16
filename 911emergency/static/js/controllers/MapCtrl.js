a46a5fcf1790c54ce42d3b7a90bb2ecca336569e
MeenakshiParyani <meenakshi.paryani@sjsu.edu>
2017-05-01 14:11:49 -0700

    Map View Stable

--- 911emergency/static/js/controllers/MapCtrl.js
+++ 911emergency/static/js/controllers/MapCtrl.js
@@ -35,10 +35,9 @@ angular.module("MapCtrl", []).controller("MapController", function ($scope, $htt
         label : 'Emergency Count'
     }
 ];
-	var bounds = new google.maps.LatLngBounds();
 	var mapOptions = {
         zoom: 10,
-        center: new google.maps.LatLng(39.974950799999995, -75.4999024)
+        center: new google.maps.LatLng(40.063168, -75.149746)
     }
 
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -52,23 +51,20 @@ angular.module("MapCtrl", []).controller("MapController", function ($scope, $htt
         var marker = new google.maps.Marker({
             map: $scope.map,
             position: new google.maps.LatLng(info.lat, info.long),
-            label: info.value
+            label: info.value,
+            title : info.label
         });
 
-        bounds.extend(marker.position);
         marker.content = '<div class="infoWindowContent">' + info.value + '</div>';
 
-        //marker.showInfoWindow();
-
-        map.fitBounds(bounds);
 
         var listener = google.maps.event.addListener(map, "idle", function () {
-		    map.setZoom(20);
+		    map.setZoom(10);
 		    google.maps.event.removeListener(listener);
 		});
         
         google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.label + '</h2>' + marker.content);
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
             infoWindow.open($scope.map, marker);
         });
         
