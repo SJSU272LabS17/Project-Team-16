2b16647c2cd82e1713cf19e606ed04d8c24f5e6e
Anup Kulkarni <Anup Kulkarni>
2017-05-05 08:50:49 -0700

    Modified to add overviewctrl under js module

--- 911emergency/static/js/app.js
+++ 911emergency/static/js/app.js
@@ -1,4 +1,3 @@
 
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'DonutCtrl', 
-	'TrendCtrl', 'MapCtrl', 'FakeCallCtrl']);
-
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'DonutCtrl',
+	'TrendCtrl', 'MapCtrl', 'FakeCallCtrl','OverviewCtrl']);
