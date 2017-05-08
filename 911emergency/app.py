ac7d6caa7780629305ad41678489f66ce3d7302f
manika15 <manika.kapoor90@gmail.com>
2017-05-07 23:59:16 -0700

    app.py changed

--- 911emergency/app.py
+++ 911emergency/app.py
@@ -15,10 +15,7 @@ import  myexception
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['BASIC_AUTH_FORCE'] = True
+app.config.from_pyfile('config.cfg')
 
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
