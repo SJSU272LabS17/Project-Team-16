369eeeb57ffa7acc1f57bec581de06981fc4a351
anupkv1 <anupkulkarni563@gmail.com>
2017-05-08 14:31:46 -0700

    modified to add video for html

--- 911emergency/static/js/app.js
+++ 911emergency/static/js/app.js
@@ -1,3 +1,3 @@
 
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'angular-loading-bar','MainCtrl', 'DonutCtrl',
-	'TrendCtrl', 'MapCtrl', 'FakeCallCtrl','OverviewCtrl','HeatmapCtrl','LoginCtrl','RegisterCtrl', 'LoginService', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+	'TrendCtrl', 'MapCtrl', 'FakeCallCtrl','OverviewCtrl','HeatmapCtrl','LoginCtrl','RegisterCtrl', 'LoginService', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache','ngVidBg']);
